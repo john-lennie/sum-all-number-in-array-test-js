3d890b6dcbdf1d06e19a9d14c8d795f8a0b58551
John Lennie <johnanthonylennie@gmail.com>
2019-04-10 17:24:17 -0400

    completed task

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // if () return; // termination case
+
+  let sum = 0;
+
+  for (var item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
